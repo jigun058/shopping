9ff28ce59a80da33cab35e4848ad5098915e1e79
jigun058 <81871941+jigun058@users.noreply.github.com>
2024-07-28 06:07:15 +0900

    Add files via upload

--- demo/src/main/java/com/DYC/demo/ItemController.java
+++ demo/src/main/java/com/DYC/demo/ItemController.java
@@ -4,22 +4,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemRepository itemRepository;
+    private final ItemService itemService;
 
     @GetMapping("/list")
     String func1(Model model){
-        //List<Item> result = itemRepository.findAll();
-        //model.addAttribute("items", result);
-        //System.out.println(result.get(0));
+        List<Item> result = itemRepository.findAll();
+        model.addAttribute("items", result);
+        System.out.println(result);
         return "list.html";
     }
 
@@ -28,23 +31,39 @@ public class ItemController {
         return "write.html";
     }
 
+    @PostMapping("/edit")
+    String editPost(Long id, String product, String description, Integer price, Integer stock, String category) {
+
+        itemService.editItem(id, product, description, price, stock, category);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable("id") Long id, Model model) {
+        Optional<Item> result = itemRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("item", result.get());
+            return "update";
+        } else {
+            return "error";
+        }
+    }
+
     @PostMapping("/add")
     String addPost(String product, String description, Integer price, Integer stock, String category) {
-        System.out.println(product);
-        System.out.println(description);
-        System.out.println(price);
-        System.out.println(stock);
-        System.out.println(category);
 
-        Item item = new Item();
-        item.product = product;
-        item.description = description;
-        item.price = price;
-        item.stock = stock;
-        item.category = category;
-
-        Item saved = itemRepository.save(item);
-        System.out.println(saved.toString());
+        itemService.saveItem(product, description, price, stock, category);
         return "redirect:/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable("id") Long id, Model model) {
+        Optional<Item> result = itemRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("data", result.get());
+            return "detail";
+        } else {
+            return "error";
+        }
     }
 }
