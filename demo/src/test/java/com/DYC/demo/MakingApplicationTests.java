be948186583caaef7032579a51728108d26a6788
jigun058 <81871941+jigun058@users.noreply.github.com>
2024-08-17 20:00:40 +0900

    Add files via upload

---
+++ demo/src/test/java/com/DYC/demo/MakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DYC.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
